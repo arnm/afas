42333616335e75c088b9c8af0b8a4e018dd176bd
arnm <alexeirnunez@gmail.com>
2019-02-01 19:16:42 -0600

    message on scoring plays

--- afas.py
+++ afas.py
@@ -24,19 +24,16 @@ CONFIG_SCHEMA = {
 
 def get_config(path, schema):
     validator = cerberus.Validator(schema)
-
     with open(path) as f:
         config = json.load(f)
-
         if not validator.validate(config):
             raise click.ClickException(
                 'invalid config: ' + str(validator.errors))
-
         return config
 
 
-def constantly_annoy(instapi, to_annoy):
-    print to_annoy
+def constantly_annoy(instapi, followers):
+    print followers
 
     def on_game_update(active, completed, diffs):
 
@@ -46,24 +43,31 @@ def constantly_annoy(instapi, to_annoy):
 
         # message opposing teams on events
         for diff in diffs:
+            # direct message from within 20yds of end zone
 
-            if [play for play in diff.plays if play.touchdown]:
-                print 'touchdown'
-                continue
-
-            if diff.before.score_away != diff.after.score_away:
-                print 'home scored'
-
-            if diff.before.score_home != diff.after.score_home:
+            # direct message on scoring play
+            touchdown_plays = [play for play in diff.plays if play.touchdown]
+            if touchdown_plays:
+                # direct message on touchdown
+                for touchdown_play in touchdown_plays:
+                    people = [person['pk']
+                              for person in followers if person['team'] == touchdown_play.team]
+                    message = 'TOUCHDOWN %s!!' % touchdown_play.team
+                    for person in people:
+                        instapi.direct_message(message, person)
+            elif diff.before.score_away != diff.after.score_away:
+                print 'away scored'
+            elif diff.before.score_home != diff.after.score_home:
                 print 'home scored'
 
         for game in completed:
-            # messsage all losers
+            # send group message to all losers
             losers = [loser['pk']
-                      for loser in to_annoy if loser['team'] == game.loser]
+                      for loser in followers if loser['team'] == game.loser]
 
-            instapi.direct_message('%s WINNNSS!!! HAHAHA BETTER LUCK NEXT TIME!' %
-                                   game.winner, losers)
+            message = '%s WINNNSS!!! HAHAHA BETTER LUCK NEXT TIME!' % game.winner
+            print 'messaging %s to %s' % (message, losers)
+            instapi.direct_message(message, losers)
 
     nflgame.live.run(on_game_update)
 
@@ -84,17 +88,18 @@ def annoy(path):
     # build easier to work with dict
     # by joining on key username
     # e.g { 'instausername': { 'username': 'instausername', pk': 12345, 'team': 'Pat' } }
-    to_annoy = {}
+    followers = {}
     for follower in config['followers']:
-        to_annoy[follower['username']] = {'team': follower['team']}
+        followers[follower['username']] = {'team': follower['team']}
 
     for follower in instapi.getTotalFollowers(instapi.username_id):
         username = follower['username']
-        if username in to_annoy:
-            to_annoy[username].update(
+        if username in followers:
+            followers[username].update(
                 {'pk': follower['pk'], 'username': username})
 
-    constantly_annoy(instapi, to_annoy.values())
+    # instapi.direct_message("asdf", [f['pk'] for f in followers.values()])
+    # constantly_annoy(instapi, followers.values())
 
 
 if __name__ == '__main__':
